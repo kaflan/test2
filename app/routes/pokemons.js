92864f5aa1d23696e66144f0c2b301b023cb176a
kaflan <df117879@gmail.com>
2016-08-25 00:31:47 +0300

    chenhe

--- app/routes/pokemons.js
+++ app/routes/pokemons.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
     model: function(){
         return new Promise(function(resolve, reject){
            return $.getJSON('http://pokeapi.co/api/v1/pokemon/?limit=12', function(resp){
-             resolve(resp);
+             resolve(resp.objects);
             console.log(resp, 'test');
            });
         }); 
