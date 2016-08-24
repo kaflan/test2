5c107f7fed8fe50e063ab121e18c9dc189791b0f
kaflan <kaf_90@mail.ru>
2016-08-24 22:46:37 +0300

    add

---
+++ app/routes/pokemons.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function(){
+        return new Promise(function(resolve, reject){
+           return $.getJSON('http://pokeapi.co/api/v1/pokemon/?limit=12', function(resp){
+             resolve(resp);
+            
+           });
+        }); 
+    }
+});
