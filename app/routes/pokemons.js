de4dcdcda02f2c1ff392fd5a7653dba0011f541d
kaflan <df117879@gmail.com>
2016-08-25 19:18:07 +0300

    help

--- app/routes/pokemons.js
+++ app/routes/pokemons.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function(){
         return new Promise(function(resolve, reject){
-           return $.getJSON('http://pokeapi.co/api/v1/pokemon/?limit=12', function(resp){
+           return $.getJSON('http://pokeapi.co/api/v1/pokemon/?limit=3', function(resp){
              resolve(resp.objects);
-            console.log(resp, 'test');
+             console.log(resp.objects, 'pokemons');
            });
-        }); 
+        });
+        // .then(function(){
+        //     return new Promise(function(resolve,reject){
+        //       return '';
+        //     });
+        // }); 
     }
 });
