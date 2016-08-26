a76b87fd3a64c8ef7ab015f59d7f230a3896fd94
kaflan <df117879@gmail.com>
2016-08-26 14:56:42 +0300

    add

--- app/components/pokemon-badge/component.js
+++ app/components/pokemon-badge/component.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    loader: Ember.inject.service('pokemon-image-loader'),
-    pokemonImage: Ember.computed('pokemon', function () {
-    const id = this.get('pokemon').national_id;
-        console.log('work', id);
-    return this.get('loader').lazyImage(id);
-    })
+    // loader: Ember.inject.service('pokemon-image-loader'),
+    // pokemonImage: Ember.computed('pokemon', function () {
+    // const id = this.get('pokemon').national_id;
+    //     console.log('work', id);
+    // return this.get('loader').lazyImage(id);
+    // })
 });
